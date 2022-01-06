913dd7d01852cbd41872ef364bdea856a930457b
leandro <leandroj.r.ximenes@gmail.com>
2022-01-06 12:12:29 -0300

    L11-end

---
+++ app/Models/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
+class Post
+{
+    public static function find($slug)
+    {
+        $path = resource_path("posts/$slug.html");
+
+        if (!file_exists($path)) {
+            throw new ModelNotFoundException();
+        }
+
+        return  cache()->remember("posts.{$slug}", now()->addMinutes(20), function () use ($path) {
+            return file_get_contents($path);
+        });
+    }
+
+    public static function all()
+    {
+        $files = File::files(resource_path("posts"));
+
+        return array_map(function ($file) {
+            return $file->getContents();
+        }, $files);
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('post/{post}', function ($slug) {
-    if (!file_exists($path = __DIR__ . "/../resources/posts/$slug.html")) {
-        // return redirect('/');
-        abort(404);
-    }
 
-    $post = cache()->remember("posts.{$slug}", now()->addMinutes(20), function () use ($path) {
-        var_dump($path);
-        return file_get_contents($path);
-    });
-
-    return view('post', ['post' => $post]);
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
 })->where('post', '[A-z_\-]+');
