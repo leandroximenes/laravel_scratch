f752aa8cae2e1d385053b2895776933649a13129
leandro-vpn <leandroj.r.ximenes@gmail.com>
2022-01-13 16:12:03 -0300

    done

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,21 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/css/style.css">
-    <script src="/js/app.js"></script>
-</head>
+@section('title')
+    {{ $post->title }}
+@endsection
 
-<body>
+@section('content')
     <article>
         <h1> <?= $post->title ?></h1>
         <?= $post->body ?>
     </article>
     <a href="/">Go Back</a>
-</body>
-
-</html>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,24 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/css/style.css">
-    <script src="/js/app.js"></script>
-</head>
+@section('title')
+    Posts
+@endsection
 
-<body>
-    <?php foreach ($posts as $key => $post) : ?>
-    <article>
-        <a href="post/<?= $post->slug ?>">
-            <h1><?= $post->title ?></h1>
-        </a>
-        <?= $post->body ?>
-    </article>
-    <?php endforeach ?>
-</body>
-
-</html>
+@section('content')
+    @foreach ($posts as $key => $post)
+        <article>
+            <a href="post/{{ $post->slug }}">
+                <h1>{{ $post->title }}</h1>
+            </a>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+< @endsection
