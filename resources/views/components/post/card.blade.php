7c6953fec8520821a1092b7c7e3ed3a0cf40f7fc
leandro <leandroj.r.ximenes@gmail.com>
2022-09-08 15:10:34 -0300

    merged with L32-end

---
+++ resources/views/components/post/card.blade.php
@@ -0,0 +1,36 @@
+@props(['post'])
+<article {{ $attributes->merge(['class' => 'p-8 hover:bg-gray-100 transition duration-300 rounded-xl']) }}>
+    <div class="flex-1 flex flex-col justify-between gap-5">
+        <main>
+            <!-- TODO: Image -->
+            <img class="rounded-xl mb-5" src="images/illustration-1.png">
+            <div>
+                <a href="{{ $post->category->slug }}"
+                    class="px-3 py-1 mr-2 border border-red-300 rounded-full uppercase text-xs text-red-300">
+                    {{ $post->category->name }}</a>
+            </div>
+            <h1 class="text-3xl tracking-wide mt-5 mb-1 font-light"><a href="/post/{{ $post->slug }}">
+                    {{ $post['title'] }}
+                </a></h1>
+            <h5 class="text-gray-400 text-xs">Published <time>{{ $post->created_at->diffForHumans() }}</time></h5>
+
+            <div class="text-justify text-md font-light mt-5">
+                <p>
+                    {{ $post->excerpt }}
+                </p>
+            </div>
+        </main>
+        <footer>
+            <div class="flex justify-between items-center">
+                <div class="flex items-center">
+                    <img src="images/lary-avatar.svg" alt="Lary avatar">
+                    <div class="ml-2">
+                        <b>{{ $post->author->name }}</b>
+                    </div>
+                </div>
+                <a class="bg-gray-200 text-xs font-semibold py-2 px-8 rounded-full transition duration-300 hover:bg-gray-300"
+                    href="/post/{{ $post->slug }}">Read More</a>
+            </div>
+        </footer>
+    </div>
+</article>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,18 @@
 <x-layout>
-    @include ('_post-header')
+    @include ('components/post/_header')
     <main class="container mx-auto max-w-screen-xl">
-        <x-post-featured-card />
-        <div class="grid lg:grid-cols-2 gap md:grid-cols-1 ">
-            <x-post-card />
-            <x-post-card />
-        </div>
-        <div class="grid lg:grid-cols-3 gap md:grid-cols-1 ">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
+        @if($posts->count())
+            <x-post.featured-card :post="$posts[0]" />
+            @if($posts->count() > 1)
+                <div class="grid lg:grid-cols-6 gap md:grid-cols-1 ">
+                    @foreach($posts->skip(1) as $post)
+                        <x-post.card 
+                            :post="$post" 
+                            class="{{ $loop->iteration > 2 ? 'col-span-2' : 'col-span-3' }}" 
+                        />
+                    @endforeach
+                </div>
+            @endif
+        @endif
     </main>
 </x-layout>
