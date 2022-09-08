cc8bfbe648ce81513f1a329fd51c5a970c0da8d7
leandro <leandroj.r.ximenes@gmail.com>
2022-09-08 15:09:59 -0300

    done'

--- resources/views/components/post/featured-card.blade.php
+++ resources/views/components/post/featured-card.blade.php
@@ -2,17 +2,20 @@
 <article class="mt-10 p-8 hover:bg-gray-100 transition duration-300 rounded-xl">
     <div class="grid lg:grid-cols-2 md:grid-cols-1 gap-8">
         <div>
-            {{-- TODO --}}
-            <img class="rounded-xl" src="/images/illustration-1.png">
+            <!-- TODO: Image -->
+            <img class="rounded-xl" src="images/illustration-1.png">
         </div>
         <div class="flex-1 flex flex-col justify-between gap-5">
             <main>
                 <div class="space-x-5">
-                    <x-category-button :category="$post->category" />
+                    <a href="{{ $post->category->slug }}"
+                        class="px-3 py-1 border border-blue-300 rounded-full font-medium uppercase text-xs text-blue-300">
+                        {{ $post->category->name }}
+                    </a>
                 </div>
                 <h1 class="text-3xl tracking-wide mt-5 mb-1 font-light">
                     <a href="/post/{{ $post->slug }}">
-                        {{ $post->title }}
+                        {{ $post['title'] }}
                     </a>
                 </h1>
                 <h5 class="text-gray-400 text-xs">Published <time>{{ $post->created_at->diffForHumans() }}</time></h5>
@@ -25,7 +28,7 @@
             </main>
             <footer class="flex justify-between items-center">
                 <div class="flex items-center">
-                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                    <img src="images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-2">
                         <b>{{ $post->author->name }}</b>
                     </div>
